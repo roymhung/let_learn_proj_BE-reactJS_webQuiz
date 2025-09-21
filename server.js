5b4745c16ca476b1a6bbade546aea640ccce3fcd
royhung <lemanhhung1555@gmail.com>
2025-09-21 12:17:21 +0700

    #17. More routes

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
-const express = require('express')
-const app = express()
-const port = 8080
+const express = require('express'); //commonjs
+// import express from 'express'; // es modules
 
+const app = express(); // app express
+const port = 8080; // port
+
+// khai bao route
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.get('/abc', (req, res) => {
+  res.send('Check ABC')
+})
+
+app.get('/royhung', (req, res) => {
+  res.send('<h1>Royhung day</h1>')
 })
 
 app.listen(port, () => {
