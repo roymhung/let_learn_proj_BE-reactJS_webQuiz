eee6986bbfa05c560c39257329e0add16ec11c50
royhung <lemanhhung1555@gmail.com>
2025-09-21 14:04:46 +0700

    #21. Static files

--- src/server.js
+++ src/server.js
@@ -1,18 +1,23 @@
 const express = require('express'); //commonjs
 const path = require('path');
+require('dotenv').config();
 // import express from 'express'; // es modules
 
+
 const app = express(); // app express
-const port = 8080; // port
+const port = process.env.PORT || 8888; // port => hardcode .uat .prod
+const hostname = process.env.HOST_NAME;
 
 //config template engine
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs')
 
+//config static files
+app.use(express.static(path.join(__dirname,'public')));
 
 // khai bao route
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World! vs nodemon hahah' )
 })
 
 app.get('/abc', (req, res) => {
@@ -23,6 +28,6 @@ app.get('/royhung', (req, res) => {
   res.render('sample.ejs')
 })
 
-app.listen(port, () => {
+app.listen(port, hostname, () => {
   console.log(`Example app listening on port ${port}`)
 })
